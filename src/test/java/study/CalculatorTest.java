77bf85aba6bfa0f56a19b4f2306fe6be3cf06c90
beautiflow <1114ny@naver.com>
2023-01-30 17:30:07 +0900

    feat: 계산기 구현 테스트

--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -1,43 +1,50 @@
 package study;
 
 import PlayGround.firstTest.Calculator;
+import PlayGround.stringCalculator.StringCalculator;
+import org.assertj.core.api.AbstractBigDecimalAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
-    Calculator cal;
+    StringCalculator stringCalculator = new StringCalculator();
 
-    @BeforeEach
-    public void setUp(){
-        cal = new Calculator();
+
+    @Test
+    @DisplayName("더하기 테스트")
+    public void addTest(){
+        assertEquals(7, stringCalculator.add(3,4));
     }
 
     @Test
-    public void 덧셈(){
-        assertEquals(7, cal.add(3,4));
+    @DisplayName("빼기 테스트")
+    public void subtractTest(){
+        assertEquals(1, stringCalculator.subtract(5,4));
     }
 
     @Test
-    public void 뺄셈(){
-        assertEquals(1, cal.substract(5,4));
+    @DisplayName("곱하기 테스트")
+    public void multiplyTest(){
+        assertEquals(6, stringCalculator.multiply(2,3));
     }
 
     @Test
-    public void 곱셈(){
-        assertEquals(6, cal.multiply(2,3));
+    @DisplayName("나누기 테스트")
+    public void divideTest(){
+        assertEquals(2, stringCalculator.divide(8,4));
     }
 
     @Test
-    public void 나눗셈(){
-        assertEquals(2, cal.divide(8,4));
+    @DisplayName("문자열 계산")
+     public void stringCalculateTest(){
+        int a = stringCalculator.stringCalculate("2 + 3 * 4 / 2");
+        assertThat(a).isEqualTo(10);
     }
 
-    @AfterEach
-    public void tearDown(){
-        cal = null;
-    }
 
 }

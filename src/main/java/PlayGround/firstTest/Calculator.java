7a8785888cb71426ff475cc85cfbd40a4b80b8d1
beautiflow <1114ny@naver.com>
2023-01-30 17:29:57 +0900

    feat: 계산기 구현

--- src/main/java/PlayGround/firstTest/Calculator.java
+++ src/main/java/PlayGround/firstTest/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
             return i + j;
         }
 
-        public int substract (int i, int j){
+        public int subtract (int i, int j){
             return i - j;
         }
 
@@ -22,12 +22,9 @@ public class Calculator {
     public static void main(String[] args) {
         Calculator cal = new Calculator();
         System.out.println(cal.add(3, 4));
-        System.out.println(cal.substract(5,4));
+        System.out.println(cal.subtract(5,4));
         System.out.println(cal.multiply(2,6));
         System.out.println(cal.divide(8,4));
     }
-
-
-
 }
 

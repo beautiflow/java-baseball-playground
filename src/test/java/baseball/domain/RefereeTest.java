c68261d9ea6a1f72c862e8612882cf1e0a2f5049
beautiflow <1114ny@naver.com>
2023-01-31 14:58:01 +0900

    refactor(baseball): 테스트 중복 제거

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Arrays;
@@ -15,31 +17,15 @@ class RefereeTest {
     private static final List<Integer> ANSWER = Arrays.asList(1, 2, 3);
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         referee = new Referee();
     }
 
-    @Test
-    void 스트라이크3() {
-        String result = referee.compare(ANSWER, Arrays.asList(1, 2, 3));
-        assertThat(result).isEqualTo("0 볼 3 스트라이크 ");
-    }
+    @ParameterizedTest
+    @CsvSource({"1,2,3, 0 볼 3 스트라이크", "7,8,9, 아웃", "2,3,1, 3 볼 0 스트라이크", "1,3,2, 2 볼 1 스트라이크"})
+    public void compare(int number1, int number2, int number3, String expected) {
+        String actual = referee.compare(ANSWER, Arrays.asList(number1, number2, number3));
+        assertThat(actual).isEqualTo(expected);
 
-    @Test
-    void 아웃() {
-        String result = referee.compare(ANSWER, Arrays.asList(7, 8, 9));
-        assertThat(result).isEqualTo("아웃");
-    }
-
-    @Test
-    void 볼3() {
-        String result = referee.compare(ANSWER, Arrays.asList(2, 3, 1));
-        assertThat(result).isEqualTo("3 볼 0 스트라이크 ");
-    }
-
-    @Test
-    void 볼2_1스트라이크(){
-        String result = referee.compare(ANSWER, Arrays.asList(1, 3, 2));
-        assertThat(result).isEqualTo("2 볼 1 스트라이크 ");
     }
 }
